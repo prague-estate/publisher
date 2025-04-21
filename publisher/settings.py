0ffc20487a8c3f955c6ffeb4f2202a031ea1e848
Simon <spam@esemi.ru>
2025-04-21 18:43:21 +0200

    :sparkles:

--- publisher/settings.py
+++ publisher/settings.py
@@ -38,6 +38,7 @@ class AppSettings(BaseSettings, extra='ignore'):
         'semrush': 14,
         'github': 7,
         'landing': 7,
+        'special': 365,
     }
 
     ENABLED_LAYOUTS: list[str] = [
